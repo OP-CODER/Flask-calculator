278d4693a0ca07c40a9610720de97224405d4c68
Anas <er.mohdanas974@gmail.com>
2025-08-11 14:15:33 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from flask import Flask, request, jsonify, render_template
-from flask_restful import Api
-from logic import Add, Subtract, Multiply, Divide
+from flask import Flask, request
 
 app = Flask(__name__)
-api = Api(app)
 
-# API endpoints
-api.add_resource(Add, "/add")
-api.add_resource(Subtract, "/subtract")
-api.add_resource(Multiply, "/multiply")
-api.add_resource(Divide, "/division")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # handle calculation
+        return "Calculation done"
+    return '''
+        <form method="POST">
+            <input type="text" name="number">
+            <input type="submit">
+        </form>
+    '''
 
-# Web UI
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True)
