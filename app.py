3dfb8451769ae12f77741a24e75f1d8386e37019
Anas <er.mohdanas974@gmail.com>
2025-08-11 14:21:19 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home():
+    result = None
     if request.method == 'POST':
-        # handle calculation
-        return "Calculation done"
-    return '''
-        <form method="POST">
-            <input type="text" name="number">
-            <input type="submit">
-        </form>
-    '''
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operation = request.form['operation']
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            result = num1 / num2 if num2 != 0 else 'Error: Division by zero'
+
+    return render_template('index.html', result=result)
